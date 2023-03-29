6be6a79b54f4895e1b63bfbed4bb420cb0a52d09
DmytroSav4uk <dmytro.savchuk.ua@gmail.com>
2023-03-29 14:46:23 +0300

    курсач жорняка

--- src/Main.java
+++ src/Main.java
@@ -1,182 +1,127 @@
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import static java.lang.Math.pow;
 
 public class Main {
-    Object[] headersA = {"Column 1", "Column 2", "Column 3", "Column 4"};
-    //Масив, що вміщає заголовки таблиці В
-    Object[] headersB = {"Column 1", "Column 2", "Column 3", "Column 4"};
-    //Масив, що вміщає заголовки перетвореної таблиці А
-    Object[] headersANew = {"Column 1", "Column 2", "Column 3", "Column 4"};
-    //Масив, що вміщає заголовки перетвореної таблиці В
-    Object[] headersBNew = {"Column 1", "Column 2", "Column 3", "Column 4"};
-    //Масив, що вміщає заголовки результуючої таблиці С
-    Object[] headersC = {"Column 1", "Column 2", "Column 3", "Column 4"};
-    Object[][] dataA = {{1, 3, 5, 5}, {2, 4, 2, 4}, {7, 9, 11, 10}, {7, 9, 8, 10},};
-    Object[][] dataB = {{71, 1, 0, 1}, {94, 2, 1, 0}, {75, 12, 31, 40}, {34, 3, 1, 2},};
-    Object[][] dataANew = new Object[4][4];
-    int i, j;
-
+    Object[] headersA = { "Column 1", "Column 2", "Column 3", "Column 4", "Column 5" };
+    Object[] headersB = { "Column 1", "Column 2", "Column 3", "Column 4", "Column 5" };
+    Object[] headersAt = { "Column 1", "Column 2", "Column 3", "Column 4" };
+    Object[] headersBt = { "Column 1", "Column 2", "Column 3", "Column 4" };
+    Object[] headersC = { "Column 1", "Column 2", "Column 3", "Column 4" };
+    Object[][] A = { { 2, 3, 4, 2, 5 }, { 3, 6, 3, 5, 7 }, { 4, 6, 3, 1, 4 }, { 6, 2, 4, 3, 5 } };
+    Object[][] B = { { 3, 2, 4, 2, 6 }, { 2, 1, 6, 5, 7 }, { 3, 6, 4, 5, 4 }, { 5, 3, 4, 2, 6 } };
+    Object[][] At = new Object[5][4];
+    Object[][] Bt = new Object[5][5];
+    Object[][] C = new Object[5][4];
+    int i, j, f, g, count;
     {
         for (i = 0; i < 4; i++)
-            for (j = 0; j < 4; j++) {
-                dataANew[i][j] = (int) dataA[j][i];
+            for (j = 0; j < 5; j++) {
+                At[j][i] = (int) A[i][j];
             }
     }
-
-    Object[][] dataBNew = new Object[4][4];
-
     {
         for (i = 0; i < 4; i++)
+            for (j = 0; j < 5; j++) {
+                Bt[j][i] = (int) B[i][j];
+            }
+    }
+
+    {
+        for (i = 0; i < 5; i++)
             for (j = 0; j < 4; j++) {
-                dataBNew[i][j] = pow((int) dataB[j][i], 1);
+                C[i][j] = 4 * ((int) At[i][j] * (int) At[i][j]) + (int) Bt[i][j];
             }
     }
-
-    //Масив, що вміщає дані таблиці C
-    Object[][] dataC = new Object[4][4];
-    int f, g;
-
     {
-        for (f = 0; f < 4; f++)
-            for (g = 0; g < 4; g++) {
-                dataC[f][g] = 7 * f + 3 * g;
-            }
-    }
-
-    int p, l, count;
-    double max, min;
-    int nr_min, ns_min;
-
-    {
-
-        nr_min = 0;
-        ns_min = 0;
-        count = 0;
-
-
-        for (p = 0; p < dataC.length; p++)
-            for (l = 0; l < dataC[p].length; l++) {
-                int foo = (int) dataC[p][l];
-                if (  foo >  0 ) {
-                    count += 1;
+        for (i = 0; i < 5; i++)
+            for (j = 0; j < 4; j++) {
+                if ((int) C[i][j] > 0) {
+                    count++;
                 }
             }
     }
-
-    int p1, l1;
     int sum = 0;
-
     {
-        for (p1 = 0; p1 < 4; p1++)
+        for (i = 1; i < 5; i++)
+            for (j = 0; j < 4; j = j + 2) {
+                sum = sum + (int) C[i][j];
 
-            for (l1 = 0; l1 < 4; l1++) {
-                if (l1 % 2 != 0){
-                sum += (int) dataC[p1][l1];
-                System.out.println(dataC[p1][l1]);
-                }
             }
-
     }
-
-    //Об'єкт таблиці А
     JTable jTabA;
-    //Об'єкт таблиці B
     JTable jTabB;
-    //Об'єкт таблиці АNew
-    JTable jTabANew;
-    //Об'єкт таблиці BNew
-    JTable jTabBNew;
-    //Об'єкт таблиці C
+    JTable jTabAt;
+    JTable jTabBt;
     JTable jTabC;
 
     Main() {
-        JFrame jfrm = new JFrame("VARIANT_10, OK-33");
+        JFrame jfrm = new JFrame("VARIANT 8, OK-33");
 
         jfrm.getContentPane().setLayout(new GridLayout(3, 2));
-        jfrm.setSize(900, 600);
+        jfrm.setSize(900, 350);
         jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jfrm.setVisible(true);
 
-        jTabA = new JTable(dataA, headersA);
+        jTabA = new JTable(A, headersA);
         JScrollPane jscrlpA = new JScrollPane(jTabA);
         jTabA.setPreferredScrollableViewportSize(new Dimension(250, 100));
-
-
         jfrm.getContentPane().add(jscrlpA);
 
-        jTabB = new JTable(dataB, headersB);
+        jTabB = new JTable(B, headersB);
         JScrollPane jscrlpB = new JScrollPane(jTabB);
         jTabB.setPreferredScrollableViewportSize(new Dimension(250, 100));
-
-
         jfrm.getContentPane().add(jscrlpB);
-        jTabANew = new JTable(dataANew, headersANew);
-        JScrollPane jscrlpANew = new JScrollPane(jTabANew);
-        jTabANew.setPreferredScrollableViewportSize(new Dimension(250, 100));
 
-        jfrm.getContentPane().add(jscrlpANew);
+        jTabAt = new JTable(At, headersAt);
+        JScrollPane jscrlpAt = new JScrollPane(jTabAt);
+        jTabAt.setPreferredScrollableViewportSize(new Dimension(250, 100));
+        jfrm.getContentPane().add(jscrlpAt);
 
-        jTabBNew = new JTable(dataBNew, headersBNew);
-        JScrollPane jscrlpBNew = new JScrollPane(jTabBNew);
-        jTabBNew.setPreferredScrollableViewportSize(new Dimension(250, 100));
+        jTabBt = new JTable(Bt, headersBt);
+        JScrollPane jscrlpBt = new JScrollPane(jTabBt);
+        jTabBt.setPreferredScrollableViewportSize(new Dimension(250, 100));
+        jfrm.getContentPane().add(jscrlpBt);
 
-        jfrm.getContentPane().add(jscrlpBNew);
-
-        jTabC = new JTable(dataC, headersC);
+        jTabC = new JTable(C, headersC);
         JScrollPane jscrlpC = new JScrollPane(jTabC);
         jTabC.setPreferredScrollableViewportSize(new Dimension(250, 100));
-
         jfrm.getContentPane().add(jscrlpC);
 
         JButton button = new JButton("Рохрахувати");
-        JLabel label2 = new JLabel("Номер рядка мінімального елементу матриці С");
-        JTextField text12 = new JTextField("              ");
-        JLabel label3 = new JLabel("Номер стовпця мінімального елементу матриці С");
-        JTextField text13 = new JTextField("              ");
-        JLabel label = new JLabel("Кількість додатних чисел матриці С");
+        JLabel label = new JLabel("Кількість елементів <0 в матриці С");
         JTextField text = new JTextField("              ");
-        JLabel label1 = new JLabel("сума елементів парних стовпців матриці  С");
+        JLabel label1 = new JLabel("Сума елементів непарних стовпчиків матриці С");
         JTextField text1 = new JTextField("                       ");
+
         FlowLayout F1 = new FlowLayout();
         JPanel panel = new JPanel(F1);
         panel.setBorder(BorderFactory.createEtchedBorder());
-
-        panel.add(label2);
-        panel.add(text12);
-        panel.add(label3);
-        panel.add(text13);
         panel.add(label);
+
         panel.add(text);
         panel.add(label1);
         panel.add(text1);
         panel.add(button);
         jfrm.getContentPane().add(panel);
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //при натисканні на кнопку виконується метод actionPerformed
-                //виводимо результат пошуку даних в матриці С
-                text12.setText("" + (nr_min + 1));
-                text13.setText("" + (ns_min + 1));
-                text.setText("" + count);
-                text1.setText("" + sum);
-            }
+        button.addActionListener(e -> {
+
+            text.setText("" + count);
+            text1.setText("" + sum);
         });
     }
 
-    //Функція main, Що запускається при старті додатка
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new Main();
-            }
-        });
+        SwingUtilities.invokeLater(Main::new);
     }
 }
